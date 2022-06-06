40ba91b3bbeb82c492dfcfc3f5ed0a13153fbc02
Raghav Bang <raghavbang2012@gmail.com>
2022-06-06 20:16:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./styles.css";
+
+// My program
+function add(a, b) {
+  return a + b;
+}
+
+// Que: write a function which takes three values. Multiply the first two and subtract the last one. Then, write a test for it.
+function threeValue(a, b, c) {
+  return a * b - c;
+}
+// My testing framework
+const pass = (testname) => console.log(testname, "passed! Yay!");
+
+const failure = (testName) => console.error(testName, "failed... :( ");
+
+function test(testName, callback) {
+  console.log("testing..", testName);
+  const testResult = callback();
+  testResult ? pass(testName) : failure(testName);
+}
+
+// My test
+test("should add two numbers", () => {
+  const a = 22;
+  const b = 11;
+
+  return add(a, b) === 33;
+});
+
+test("should add firsttwo and subtract third", () => {
+  const a = 10;
+  const b = 20;
+  const c = 30;
+  return threeValue(a, b, c) === 170;
+});
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="app-header">tanaypratap's box</h1>
+      <div className="app-body">Put your app body here</div>
+    </div>
+  );
+}
